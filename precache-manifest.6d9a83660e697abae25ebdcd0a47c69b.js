65f93f5a5d2490bc799f98b747227631eb3cb500
Vignesh Marimuthu <vigneshmarimuthu2302@gmail.com>
2020-09-02 22:49:24 +0530

    added source

---
+++ precache-manifest.6d9a83660e697abae25ebdcd0a47c69b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc96ac53ed2e9f2422983a4e079d62f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bcf585aa7e01e5092ac",
+    "url": "/static/css/2.89946ceb.chunk.css"
+  },
+  {
+    "revision": "57c3aaa1ef0aac72f9f0",
+    "url": "/static/css/main.3e2a7090.chunk.css"
+  },
+  {
+    "revision": "8bcf585aa7e01e5092ac",
+    "url": "/static/js/2.4a2302d1.chunk.js"
+  },
+  {
+    "revision": "e18a5cd7c183801c474d96ead7ab1433",
+    "url": "/static/js/2.4a2302d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57c3aaa1ef0aac72f9f0",
+    "url": "/static/js/main.4ba760cf.chunk.js"
+  },
+  {
+    "revision": "13bb1fe16f42feec169f",
+    "url": "/static/js/runtime-main.6ab6062a.js"
+  },
+  {
+    "revision": "e083723dc1d620e73e742c2dc5d5f8d9",
+    "url": "/static/media/prj1.e083723d.webp"
+  },
+  {
+    "revision": "34e893eda7d1f0f0e426feaae2abeaba",
+    "url": "/static/media/prj2.34e893ed.webp"
+  },
+  {
+    "revision": "a758160cd51c5cd696f0dc8520b36b65",
+    "url": "/static/media/prj3.a758160c.webp"
+  },
+  {
+    "revision": "d9c13456f90090c425c48201e1e13ce1",
+    "url": "/static/media/prj4.d9c13456.webp"
+  },
+  {
+    "revision": "dce3319d5a5b270eba57c6876ba89560",
+    "url": "/static/media/prj5.dce3319d.webp"
+  },
+  {
+    "revision": "0ab387abff0845e8a82b54b26bfcecd7",
+    "url": "/static/media/prj6.0ab387ab.webp"
+  },
+  {
+    "revision": "1a33e9ae9c826996d6bc38c4fa44673a",
+    "url": "/static/media/profile.1a33e9ae.webp"
+  }
+]);
